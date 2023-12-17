fcc752d7905197344e8ffbe568e89ae13bcf25f7
= <=>
2023-12-17 22:24:26 +0530

    feat: updated pages

---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,10 @@
+const ArticlesListPage = () => {
+    return (
+      <div>
+        <h1>Articles List Page</h1>
+        <p>This is the articles list page</p>
+      </div>
+    );
+  }
+  
+  export default ArticlesListPage;
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+    return (
+      <div>
+        <h1>Not Found Page</h1>
+        <p>This is the not found page</p>
+      </div>
+    );
+  }
+  
+  export default NotFoundPage;
