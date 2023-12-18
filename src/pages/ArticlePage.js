dab7c79c739c1a464a41df1e0ea123ff2fa32639
= <=>
2023-12-18 13:02:50 +0530

    fix: fix console error

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -8,8 +8,8 @@ const ArticlePage = () => {
   return (
     <>
     <h1>{article.title}</h1>
-    {article.content.map((paragraph, key) => (
-      <p key={key}>{paragraph}</p>
+    {article.content.map((paragraph, i) => (
+      <p key={i}>{paragraph}</p>
     ))}
     </>
     );
