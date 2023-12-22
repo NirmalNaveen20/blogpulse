bfea12e57d2aee851eef82198fa4e62f14683879
= <=>
2023-12-22 21:58:30 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 import fs from 'fs';
+import path from 'path';
 import admin from 'firebase-admin';
 import express from 'express';
+import 'dotenv/config';
 import { db, connectToDb } from './db.js';
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const credentials = JSON.parse(
     fs.readFileSync('./credentials.json')
@@ -12,20 +18,27 @@ admin.initializeApp({
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build')));
 
-app.use(async(req, res, next) => {
+app.get(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+})
+
+app.use(async (req, res, next) => {
     const { authtoken } = req.headers;
 
     if (authtoken) {
         try {
             req.user = await admin.auth().verifyIdToken(authtoken);
         } catch (e) {
-           return res.sendStatus(400);
+            return res.sendStatus(400);
         }
     }
+
     req.user = req.user || {};
+
     next();
-})
+});
 
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
@@ -48,7 +61,7 @@ app.use((req, res, next) => {
     } else {
         res.sendStatus(401);
     }
-})
+});
 
 app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
@@ -59,16 +72,15 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
     if (article) {
         const upvoteIds = article.upvoteIds || [];
         const canUpvote = uid && !upvoteIds.includes(uid);
-
+   
         if (canUpvote) {
             await db.collection('articles').updateOne({ name }, {
                 $inc: { upvotes: 1 },
-                $push: {upvoteIds: uid}
+                $push: { upvoteIds: uid },
             });
         }
-        
-        const updatedArticle = await db.collection('articles').findOne({ name });
 
+        const updatedArticle = await db.collection('articles').findOne({ name });
         res.json(updatedArticle);
     } else {
         res.send('That article doesn\'t exist');
@@ -92,9 +104,11 @@ app.post('/api/articles/:name/comments', async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 8000;
+
 connectToDb(() => {
     console.log('Successfully connected to database!');
-    app.listen(8000, () => {
-        console.log('Server is listening on port 8000');
+    app.listen(PORT, () => {
+        console.log('Server is listening on port ' + PORT);
     });
 })
